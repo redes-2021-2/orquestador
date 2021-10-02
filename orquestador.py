ce778c59f048e5c9844ced97e91b3b38b91dfe60
alexhdz1 <42750928+alexhdz1@users.noreply.github.com>
2021-10-02 16:04:59 -0500

    Add files via upload

---
+++ orquestador.py
@@ -0,0 +1,22 @@
+from typing import cast
+import requests
+from decouple import config
+import json as js
+
+URL_1 = config("LOCALHOST", cast=str)
+URL_2 = config("LOCALHOST", cast=str)
+
+
+i = 0
+valor = 0
+while i < 50:
+    if :
+        response = requests.post(URL_1+"/suma", json={"variable": valor})
+        valor = response.json().get("variable")
+        print(valor)
+        i = i + 1
+    else:
+        response = requests.post(URL_2+"/suma", json={"variable": valor})
+        valor = response.json().get("variable")
+        print(valor)
+        i = i + 1
